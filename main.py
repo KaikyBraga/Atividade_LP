5fbf497179825fcab8174a6c964a9d427ed649aa
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-10-30 22:12:01 -0300

    Adicionando DocStrings (comentários)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 class Company(Enum):
+    """
+    Representa empresas disponíveis no sistema.
+
+    Esta enumeração permite associar um nome a uma empresa por meio de um valor numérico.
+    """
     COCACOLA = 1
     PEPSICO = 2
     APPLE = 3
@@ -9,7 +14,22 @@ class Company(Enum):
     GUARANA = 6
 
 class Product:
+    """
+    Representa um produto genérico.
+
+    Esta classe contém atributos comuns a todos os produtos e métodos.
+    """
     def __init__(self, name_product: str, barcode: str, price: float, id_company: int, category: str):
+        """
+        Inicializa um novo produto.
+
+        Args:
+            name_product (str): Nome do produto.
+            barcode (str): Código de barras do produto.
+            price (float): Preço do produto.
+            id_company (int): ID da empresa que fabrica o produto.
+            category (str): Categoria do produto.
+        """
         self.name_product = name_product
         self.barcode = barcode
         self.price = price
@@ -17,6 +37,12 @@ class Product:
         self.category = category
 
     def get_info_product(self):
+        """
+        Retorna informações detalhadas sobre o produto.
+
+        Returns:
+            dict: Um dicionário com informações do produto, incluindo nome, código de barras, preço, empresa e categoria.
+        """
         return {"Product Name": self.name_product, 
                 "Barcode": self.barcode, 
                 "Price": self.price, 
@@ -25,44 +51,146 @@ class Product:
                 "Category": self.category}
     
     def update_name(self, _name_product):
+        """
+        Atualiza o nome do produto.
+
+        Args:
+            _name_product (str): Novo nome do produto.
+        """
         self.name_product = _name_product
 
     def update_barcode(self, _barcode):
+        """
+        Atualiza o código de barras do produto.
+
+        Args:
+            _barcode (str): Novo código de barras.
+        """
         self.barcode = _barcode
 
     def update_price(self, _price):
+        """
+        Atualiza o preço do produto.
+
+        Args:
+            _price (float): Novo preço do produto.
+        """
         self.price = _price
 
     def update_id_company(self, _id_company):
+        """
+        Atualiza o ID da empresa que fabrica o produto.
+
+        Args:
+            _id_company (int): Novo ID da empresa.
+        """
         self.id_company = _id_company
 
     def update_category(self, _category):
+        """
+        Atualiza a categoria do produto.
+
+        Args:
+            _category (str): Nova categoria do produto.
+        """
         self.category = _category
     
 class ClothingProduct(Product):
+    """
+    Representa um produto de roupas.
+
+    Esta classe herda os atributos e métodos da classe 'Product' e adiciona atributos específicos para roupas.
+    """
     def __init__(self, name_product, barcode, price, id_company, category, size, style):
+        """
+        Inicializa um novo produto de roupas.
+
+        Args:
+            name_product (str): Nome do produto.
+            barcode (str): Código de barras do produto.
+            price (float): Preço do produto.
+            id_company (int): ID da empresa que fabrica o produto.
+            category (str): Categoria do produto.
+            size (str): Tamanho das roupas.
+            style (str): Estilo das roupas.
+        """
         super().__init__(name_product, barcode, price, id_company, category)
         self.size = size
         self.style = style
 
 class EletronicProduct(Product):
+    """
+    Representa um produto eletrônico.
+
+    Esta classe herda os atributos e métodos da classe 'Product' e adiciona atributos específicos para produtos eletrônicos.
+    """
     def __init__(self, name_product, barcode, price, id_company, category, warranty):
+        """
+        Inicializa um novo produto eletrônico.
+
+        Args:
+            name_product (str): Nome do produto.
+            barcode (str): Código de barras do produto.
+            price (float): Preço do produto.
+            id_company (int): ID da empresa que fabrica o produto.
+            category (str): Categoria do produto.
+            warranty (str): Informações sobre a garantia do produto eletrônico.
+        """
         super().__init__(name_product, barcode, price, id_company, category)
         self.warranty = warranty
 
 class FoodProduct(Product):
+    """
+    Representa um produto alimentício.
+
+    Esta classe herda os atributos e métodos da classe 'Product' e adiciona atributos específicos para produtos alimentícios.
+    """
     def __init__(self, name_product, barcode, price, id_company, category, expiration_date):
+        """
+        Inicializa um novo produto alimentício.
+
+        Args:
+            name_product (str): Nome do produto.
+            barcode (str): Código de barras do produto.
+            price (float): Preço do produto.
+            id_company (int): ID da empresa que fabrica o produto.
+            category (str): Categoria do produto.
+            expiration_date (str): Data de validade do produto alimentício.
+        """
         super().__init__(name_product, barcode, price, id_company, category)
         self.expiration_date = expiration_date
 
 class Invetory():
+    """
+    Representa um inventário de produtos.
+
+    Esta classe permite adicionar, remover e listar produtos no inventário.
+    """
     def __init__(self):        
-        self.products_list = [] # [[obj1, qnt1], [obj2, qnt2], [obj3, qnt]]
+        """Inicializa o inventário vazio."""
+        self.products_list = [] 
 
     def get_products_list(self):
+        """
+        Retorna a lista de produtos no inventário.
+
+        Returns:
+            list: Uma lista de produtos no inventário.
+        """
         return self.products_list
 
     def add_product(self, product, qnt: int):
+        """
+        Adiciona uma quantidade específica de um produto ao inventário.
+
+        Esta função permite adicionar uma quantidade específica de um produto ao inventário. Ela verifica se o tipo de produto
+        é válido (FoodProduct, ClothingProduct ou EletronicProduct) e, em seguida, procura o produto na lista do inventário.
+        Se o produto já estiver no inventário, a quantidade disponível é incrementada. Caso contrário, um novo item é adicionado.
+
+        Args:
+            product (Product): O produto a ser adicionado.
+            qnt (int): A quantidade a ser adicionada.
+        """
         if(type(product)!=FoodProduct and type(product)!=ClothingProduct and type(product)!=EletronicProduct):
             raise TypeError("Objeto invalido")
 
@@ -78,10 +206,27 @@ class Invetory():
             self.products_list.append([product, qnt])
 
     def show_invetory(self):
+        """
+        Exibe a lista de produtos no inventário com suas informações.
+
+        Esta função imprime as informações de todos os produtos no inventário, incluindo nome, código de barras, preço,
+        quantidade em estoque, empresa e categoria. Cada produto é listado com a quantidade disponível.
+        """
         for prod in self.products_list:
             print(prod[0].get_info_product(), prod[1])
 
     def rmv_product(self, product, qnt: int):
+        """
+        Remove uma quantidade específica de um produto do inventário.
+
+        Esta função permite remover uma quantidade específica de um produto do inventário. O tipo de produto é verificado
+        e a quantidade disponível é atualizada. Se a quantidade especificada for maior do que a disponível, uma exceção
+        é gerada.
+
+        Args:
+            product (Product): O produto a ser removido.
+            qnt (int): A quantidade a ser removida.
+        """
         if(type(product)!=FoodProduct and type(product)!=ClothingProduct and type(product)!=EletronicProduct):
             raise TypeError("Objeto invalido")
 
@@ -102,21 +247,4 @@ class Invetory():
         
         if flag:
             raise Exception("Objeto nao encontrado")
-
-    
-guarana_lata = FoodProduct("Guarana Lata", "3128939120381", 3.70, 6, "Refrigerante", "24-12-05")
-coca_lata = FoodProduct("Coca-cola Lata", "31289392320381", 3.50, 1, "Refrigerante", "24-10-30")
-iphone20 = FoodProduct("Iphone 20s", "31243250949085", 15000, 3, "Smartphone", 2)
-
-estoque = Invetory()
-estoque.add_product(guarana_lata, 100)
-estoque.add_product(coca_lata, 180)
-estoque.add_product(coca_lata, 50)
-estoque.add_product(guarana_lata, 10)
-
-estoque.rmv_product(coca_lata, 50)
-estoque.rmv_product(coca_lata, 130)
-#estoque.rmv_product(iphone20, 50)
-
-estoque.rmv_product(coca_lata, 50)
-estoque.show_invetory()
+        
